4d6fa988a0af37086f6f5a0f06b398c3f1810205
ANBURAJA R <anburaja1296@gmail.com>
2026-08-13 18:40:59 +0530

    cprogram_1.c

--- cprogram_1.c
+++ cprogram_1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    FILE *fp;
+    // Opening A File In C Programming !
+    FILE *fpt;
     char anbu[100];
-    fp = fopen("p_files\\file1.txt","w");
+    fpt = fopen("p_files\\file1.txt","w");
     printf("Enter A String:");
     gets(anbu);
     for (int i = 0;i<= strlen(anbu); i++) {
-     fputc(anbu[i] ,fp);
+     fputc(anbu[i] ,fpt);
     }
     
-    if(fp==NULL){
+    if(fpt==NULL){
         printf("File Not created !");
     }
     else {
         printf("File Created Sucessfully !");
     }
-    fclose(fp);
+    fclose(fpt);
 }
