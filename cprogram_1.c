5dc4948c099180a79a92933f2104901d66fac948
ANBURAJA R <anburaja1296@gmail.com>
2026-08-13 18:29:01 +0530

    cprogram_1.c

---
+++ cprogram_1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+    FILE *fp;
+    char anbu[100];
+    fp = fopen("p_files\\file1.txt","w");
+    printf("Enter A String:");
+    gets(anbu);
+    for (int i = 0;i<= strlen(anbu); i++) {
+     fputc(anbu[i] ,fp);
+    }
+    
+    if(fp==NULL){
+        printf("File Not created");
+    }
+    else {
+        printf("File Created Sucessfully");
+    }
+    fclose(fp);
+}
